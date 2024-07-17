d8ad28e2ae4feb0c67317a1079eb8e725cd7a4d5
Pranav Anbarasu <pranavanba@gmail.com>
2024-07-17 23:37:01 +0000

    Remove unused config params and their references

--- scripts/fetch-data.R
+++ scripts/fetch-data.R
@@ -10,10 +10,6 @@ cat("\n----Fetching data----\n")
 login <- synapser::synLogin()
 
 # Get input files from synapse
-concept_map <- 
-  recoverutils::syn_file_to_df(ontologyFileID, "CONCEPT_CD") %>% 
-  filter(CONCEPT_CD!="<null>")
-
 selected_vars <- 
   recoverutils::syn_file_to_df(selectedVarsFileID) %>% 
   mutate(Lower_Bound = suppressWarnings(as.numeric(Lower_Bound)),
@@ -35,7 +31,7 @@ bucket_path <-
   paste0(
     token$bucket, '/', 
     token$baseKey, '/', 
-    stringr::str_extract(s3basekey, stringr::regex("[\\d]{4}-[\\d]{2}-[\\d]{2}")), '/'
+    stringr::str_extract(archiveVersion, stringr::regex("[\\d]{4}-[\\d]{2}-[\\d]{2}")), '/'
   )
   
 s3 <- 
@@ -59,11 +55,8 @@ for (dataset in dataset_list) {
   dataset_paths <- c(dataset_paths, dataset$path)
 }
 
-# For use in process-data steps
-concept_replacements_reversed <- recoverutils::vec_reverse(concept_replacements)
-
-if (!dir.exists(outputConceptsDir)) {
-  dir.create(outputConceptsDir)
+if (!dir.exists(outputDataDir)) {
+  dir.create(outputDataDir)
 }
 
 cat("----Finished----\n")
